95a2c49fa93d838dae02bde5bccd5d89e0f5b9f6
guiguisch067 <guigues0067@hotmail.fr>
2023-01-10 20:52:11 +0100

    test_un-commit

--- Morpion/morpion_matrice.py
+++ Morpion/morpion_matrice.py
@@ -3,7 +3,6 @@
 morpion = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 joueur = ""
 nbtour = 0
-test = "test"
 
 
 def afficher(): # une fonction bidon pour éviter d'utiliser cette horreur à chaque affichage
