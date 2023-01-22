f4739715b86bc1c85cad42b0df9bf5a026a15233
guiguisch067 <guigues0067@hotmail.fr>
2023-01-22 21:55:40 +0100

    galere encore et toujours

--- Morpion/morpion_csv.py
+++ Morpion/morpion_csv.py
@@ -8,25 +8,29 @@ import sys
 # Création du morpion vide
 
 # data rows of csv file
-rows = [ ['-', '-', '-'],
+rows: list[list] = [ ['-', '-', '-'],
         ['-', '-', '-'],
         ['-', '-', '-'] ]
 # name of csv file
 morpion: str = "morpion.csv"
+# boule
+i: int = 0
 # writing to csv file
 with open(morpion, 'w') as csvfile:
     # creating a csv writer object
-    csvwriter = csv.writer(csvfile, delimiter=',', lineterminator='\n')
+    csvwriter = csv.writer(csvfile, delimiter=',', lineterminator='\n', )
     # writing the data rows
     csvwriter.writerows(rows)
-    print("Quel est le joueur ? X ou O ")
-    joueur = str(input())
-    print("quelle est la position à jouer ? col : ")
-    col = int(input())
-    print("quelle est la position à jouer ? line : ")
-    line = int(input())
-    rows[line][col] = joueur
-    csvwriter.writerow(rows)
+    while i in range(2): #devrait etre 9, tests temporaires
+        print("Quel est le joueur ? X ou O ")
+        joueur = str(input())
+        print("quelle est la position à jouer ? col : ")
+        col = int(input())
+        print("quelle est la position à jouer ? line : ")
+        line = int(input())
+        rows[line][col] = joueur
+        csvwriter.writerow(rows)
+        i += 1
 
 
 def affiche_morpion(morpion: str):
